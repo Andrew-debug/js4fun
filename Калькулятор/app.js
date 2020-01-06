198609df4bad1001affd6708cbaee14960fe7041
andrew.yatsenko <1solo4ndrew88@gmail.com>
2020-01-06 18:12:06 +0200

    qwer

---
+++ Калькулятор/app.js
@@ -0,0 +1,22 @@
+function kek1() {
+    let shit1 = document.getElementById('num1').value;
+    let shit2 = document.getElementById('num2').value;
+
+    shit1 = parseInt(shit1);
+    shit2 = parseInt(shit2);
+
+    result = shit1+shit2;
+
+    document.getElementById('result').innerHTML = result
+}
+function kek2() {
+    let shit1 = document.getElementById('num1').value;
+    let shit2 = document.getElementById('num2').value;
+
+    shit1 = parseInt(shit1);
+    shit2 = parseInt(shit2);
+
+    result = shit1-shit2;
+
+    document.getElementById('result').innerHTML = result
+}
